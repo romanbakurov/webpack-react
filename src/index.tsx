8f6f60598f34523eb6415019e27b9acefd7c83db
Roman Bakurov <ibakur@yandex.ru>
2024-10-19 13:07:55 +0300

    two commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,5 @@
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/dog.jpg';
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
 
-console.log(style); 
-
-
-
-const numbers:number[] = [2, 3, 5];
-
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
+console.log(style)
